4aa809079673e004f0d447955962486c2a647c4f
nkyungm <skarudals27@naver.com>
2022-01-31 23:22:57 +0900

    BaekJoon_09,10

--- Baekjoon/BaekJoon_09_01.py
+++ Baekjoon/BaekJoon_09_01.py
@@ -1,7 +1,14 @@
-def factorial(n):
-    if(n<=1):
-        return 1
-    else:
-        return n*factorial(n-1)
 n=int(input())
-print(factorial(n))
+count=0
+c=0
+a=list(map(int,input().split()))
+for i in range(n):
+    c=0
+    if(a[i]==1):
+        continue
+    for j in range(2,a[i]):
+        if(a[i]%j==0):
+            c+=1
+    if(c==0):
+        count+=1
+print(count)
