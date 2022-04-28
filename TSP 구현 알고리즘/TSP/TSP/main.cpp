62b8dce6a19f2d55dca71018dc3dcd07330378cd
nkyungm <skarudals27@naver.com>
2022-04-28 23:31:31 +0900

    TSP 구현

---
+++ TSP 구현 알고리즘/TSP/TSP/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+void travel(int n, const int **W, int **P, int& minlength)
+{
+	int i, j, k; //¿Œµ¶Ω∫
+	int **D;
+	D = (int**)malloc(sizeof(int*) * n);
+	for (int i = 0; i < n; i++) {
+		D[i] = (int*)malloc(sizeof(int)*(int)(pow((double)2,(double)(n-1))));
+	}
+
+
+
+}
+int main() {
+
+}
