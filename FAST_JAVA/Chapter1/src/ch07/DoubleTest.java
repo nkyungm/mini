12785796a1d27171faaf8f1d6e3e9eac4c7cd102
nkyungm <skarudals27@naver.com>
2022-05-21 22:30:09 +0900

    java

---
+++ FAST_JAVA/Chapter1/src/ch07/DoubleTest.java
@@ -0,0 +1,14 @@
+package ch07;
+
+public class DoubleTest {
+
+	public static void main(String[] args) {
+		
+		double dnum=3.14; 
+		float fnum=3.14f; //double로 저장->f 적어줘야함
+		
+		System.out.println(dnum);
+		System.out.println(fnum);
+	}
+
+}
---
+++ FAST_JAVA/Chapter1/src/ch10/ContantTest.java
@@ -0,0 +1,16 @@
+package ch10;
+
+public class ContantTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int MAX_NUM=200;
+		final int MIN_NUM;
+		
+		MIN_NUM=0;
+		
+		System.out.println(MAX_NUM);
+		System.out.println(MIN_NUM);
+	}
+
+}
